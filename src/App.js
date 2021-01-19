cbee1f513f99f3601ebf6dc39042ed2b1fb50d57
gy.szvitek <gy.szvitek@gmail.com>
2021-01-19 01:43:24 +0100

    feat(components): folder structure setup, dependencies added, remove unused files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from "./images/memories.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Memories</Typography>
+		<img src={memories} height="300" alt="memories"/>
+      </AppBar>
+	  <Grow in>
+		  <Container>
+			<Grid container justify="space-between" alignItems="stretch" spacing={3}>
+				<Grid item xs={12} sm={7}>
+					<Posts />
+				</Grid>
+				<Grid item xs={12} sm={4}>
+					<Form />
+				</Grid>
+			</Grid>
+		  </Container>
+	  </Grow>
+    </Container>
   );
 }
 
