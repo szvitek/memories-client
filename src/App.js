55c30900f74815d764463dcdbd2b224bd8980518
gy.szvitek <gy.szvitek@gmail.com>
2021-01-21 00:26:48 +0100

    feat(styles): added some nice stylings

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import memories from "./images/memories.png";
+import useStyles from './styles';
 
 function App() {
-  return (
+	const classes = useStyles();
+
+    return (
     <Container maxidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Memories</Typography>
-		<img src={memories} height="300" alt="memories"/>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
+		<img className={classes.image} src={memories} height="60" alt="memories"/>
       </AppBar>
 	  <Grow in>
 		  <Container>
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import useStyles from './styles';
 
 function Post() {
+	const classes = useStyles();
 	return (
 		<h1>POST</h1>
 	);
