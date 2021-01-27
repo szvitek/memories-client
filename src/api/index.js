4a9451e41741b3f02b1a1e091c70b44a090d2122
gy.szvitek <gy.szvitek@gmail.com>
2021-01-27 23:57:50 +0100

    feat(Posts): delete posts functionality added

--- src/api/index.js
+++ src/api/index.js
@@ -5,5 +5,6 @@ import axios from 'axios';
 const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
+export const createPost = newPost => axios.post(url, newPost);
 export const updatePost = (id, postData) => axios.patch(`${url}/${id}`, postData);
+export const deletePost = id => axios.delete(`${url}/${id}`);
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -5,9 +5,13 @@ import DeleteIconIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+import { deletePost } from '../../../actions/posts';
 
 function Post({ post, setCurrentId }) {
 	const classes = useStyles();
+	const dispatch = useDispatch();
+
 	return (
 		<Card className={classes.card}>
 			<CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
@@ -37,7 +41,7 @@ function Post({ post, setCurrentId }) {
 					Like
 					{post.likeCount}
 				</Button>
-				<Button size="small" color="primary" onClick={() => {}}>
+				<Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))}>
 					<DeleteIconIcon fontSize="small" />
 					Delete
 				</Button>
