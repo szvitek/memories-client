aee4b19f792de83f54c0a4b161b2aa01efe76785
gy.szvitek <gy.szvitek@gmail.com>
2021-01-28 00:24:52 +0100

    feat(Posts): like posts functionality added

--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -6,7 +6,8 @@ const reducer = (posts = [], action) => {
 		case 'CREATE': {
 			return [...posts, action.payload];
 		}
-		case 'UPDATE': {
+		case 'UPDATE':
+		case 'LIKE': {
 			return posts.map(post => post._id === action.payload._id ? action.payload : post);
 		}
 		case 'DELETE': {
