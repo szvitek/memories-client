a83318c5e94550545ca46b9f538a3c0411d95c3a
gy.szvitek <gy.szvitek@gmail.com>
2021-01-29 23:40:23 +0100

    feat(redux): using action constants

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,3 +1,4 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 import * as api from '../api';
 
 // action creators:
@@ -6,7 +7,7 @@ import * as api from '../api';
 export const getPosts = () => async (dispatch) => {
 	try {
 		const { data } = await api.fetchPosts();
-		const action = { type: 'FETCH_ALL', payload: data };
+		const action = { type: FETCH_ALL, payload: data };
 		dispatch(action);
 	} catch (error) {
 		console.log(error.message);
@@ -16,7 +17,7 @@ export const getPosts = () => async (dispatch) => {
 export const createPost = (post) => async (dispatch) => {
 	try {
 		const { data } = await api.createPost(post);
-		dispatch({ type: 'CREATE', payload: data })
+		dispatch({ type: CREATE, payload: data })
 	} catch (error) {
 		console.log(error.message);
 	}
@@ -25,7 +26,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
 	try {
 		const { data } = await api.updatePost(id, post);
-		dispatch({ type: 'UPDATE', payload: data });
+		dispatch({ type: UPDATE, payload: data });
 	} catch (error) {
 		console.log(error.message);
 	}
@@ -34,7 +35,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = id => async dispatch => {
 	try {
 		await api.deletePost(id);
-		dispatch({ type: 'DELETE', payload: id });
+		dispatch({ type: DELETE, payload: id });
 	} catch (error) {
 		console.log(error.message);
 	}
@@ -44,7 +45,7 @@ export const likePost = id => async dispatch => {
 	try {
 		const { data } = await api.likePost(id);
 
-		dispatch({ type: 'LIKE', payload: data })
+		dispatch({ type: LIKE, payload: data })
 	} catch (error) {
 		console.log(error.message);
 	}
